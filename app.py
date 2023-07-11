9df1d54c7866f50360beae5b53c948ff5bb52479
mishi93999 <134986097+mishi93999@users.noreply.github.com>
2023-07-11 12:14:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ import csv
 import pandas as pd
 from pandas import DataFrame
 import numpy as np
+import gensim
+import matplotlib.pyplot as plt
+import nltk
+import pyLDAvis.gensim_models
+import regex
+import seaborn as sns
+import streamlit as st
+from gensim import corpora
+from gensim.models import CoherenceModel
+from gensim.utils import simple_preprocess
+
+import streamlit.components.v1 as components
+import matplotlib.colors as mcolors
+import plotly.express as px
